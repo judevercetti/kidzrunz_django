800008e2632d8f2352b9e698063f520330561507
judevercetti <judevercetti@gmail.com>
2023-01-01 19:30:07 +0300

    rollback to sqlite

--- config/settings.py
+++ config/settings.py
@@ -90,18 +90,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kidzrunz',
-        'USER': 'kidzrunz_web',
-        'PASSWORD': 'Kidzrunz@web',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'kidzrunz',
+    #     'USER': 'kidzrunz_web',
+    #     'PASSWORD': 'Kidzrunz@web',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
